242f5f7f8f7f5f01c3f95dc9993b539b1b755f83
bisht230 <bishtdeepanshu007@gmail.com>
2022-12-24 11:55:14 +0530

    Designing the chat interface main body

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import './chat.css'
 import ChatHeader from './chatHeader'
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import GifIcon from '@mui/icons-material/Gif';
 const Chat = () => {
   return (
     <div className='chat'>
         <ChatHeader />
         <div className='chat__messages'>
-
+            
         </div>
         <div className='chat__input'>
-
+        <AddCircleIcon />
+        <form>
+            <input placeholder={`Message #test`}></input>
+            <button className='chat__inputButton' type='submit'>Send Message</button>
+        </form>
+        <div className='chat__inputIcons'>
+           <CardGiftcardIcon fontSize='large' />
+           <GifIcon fontSize='large' />
+           <EmojiEmotionsIcon fontSize='large'/>
+        </div>
         </div>
         </div>
   )
